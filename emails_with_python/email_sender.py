62f93518adafe79c5dcd6109804282bb031a619a
WhoopsDang <1190518@alumno.um.edu.mx>
2023-08-08 18:40:55 -0600

    email and password finish

--- emails_with_python/email_sender.py
+++ emails_with_python/email_sender.py
@@ -3,14 +3,14 @@ from email.message import EmailMessage
 from string import Template
 from pathlib import Path
 
-html = Template(Path('index.html').read_text())
+html = Template(Path("index.html").read_text())
 email = EmailMessage()
 email["from"] = "Mat"
 email["to"] = "foxherrera01@gmail.com"
 email["subject"] = "YOU WON 1,000,000 DOLLARS!"
 
 
-email.set_content(html.substitute({'name':'TinTin'}))
+email.set_content(html.substitute({"name": "TinTin"}), "html")
 
 with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
     smtp.ehlo()
