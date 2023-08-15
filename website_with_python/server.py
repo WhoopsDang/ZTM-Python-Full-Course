780fd9952b2bc38dec1245ae28211fa14570e3a8
WhoopsDang <1190518@alumno.um.edu.mx>
2023-08-14 18:38:07 -0600

    Website changes

--- website_with_python/server.py
+++ website_with_python/server.py
@@ -1,19 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import csv
 
 app = Flask(__name__)
 app.testing = True
 
 
-@app.route("/<username>")
-def hello_world(username=None):
-    return render_template("index.html", name=username)
+@app.route("/")
+def my_home():
+    """Default route for site"""
+    return render_template("index.html")
 
 
-@app.route("/blog/2020/dogs")
-def blog():
-    return "this is a dog!"
+@app.route("/<string:page_name>")
+def basic_routes(page_name=None):
+    return render_template(page_name)
 
 
-@app.route("/<username>/<int:post_id:>")
-def params(username=None, post_id=None):
-    return render_template("index.html", name=username, post_id=post_id)
+def write_to_file(data):
+    with open("database.txt", mode="a") as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f"\n{email}, {subject}, {message}")
+
+
+def write_to_csv(data):
+    with open("database.csv", newline="", mode="a") as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(
+            database, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+        )
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route("/submit_form", methods=["POST", "GET"])
+def submit_form():
+    try:
+        if request.method == "POST":
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect("/thankyou.html")
+    except:
+        return "did not save to database"
+    else:
+        return "someting went wrong"
